dcc7c0ef21641f35da44059d9a73f354278f6b8e
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-02-20 16:02:35 +0900

    feat : onSubmit 로그인/회원가입 분기시키기

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,10 +1,56 @@
+import { useState } from "react";
+
 const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+
+    const onChange = (event) => {
+        const {
+            target: { name, value },
+        } = event;
+
+        // name에 따라 입력시켜주기
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
+
+    const onSubmit = (event) => {
+        // 새로고침을 막아줌
+        event.preventDefault();
+        if (newAccount) {
+            // create New Acc
+        } else {
+            // Log in
+        }
+    };
+
     return (
         <div>
-            <form>
-                <input type="email" placeholder="Email" required />
-                <input type="password" placeholder="Password" required />
-                <input type="submit" value="Log In" />
+            <form onSubmit={onSubmit}>
+                <input
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={onChange}
+                    required
+                />
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={onChange}
+                    required
+                />
+                <input
+                    type="submit"
+                    value={newAccount ? "Create Account" : "Log In"}
+                />
             </form>
             <div>
                 <button>Contunue With Google</button>
