e7db8634827a0e9c09c27540a4f79365cd858f84
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-02-20 15:04:43 +0900

    refactor : usestate app.js로 이동

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -1,7 +1,14 @@
 import AppRouter from "./Router";
+import { useState } from "react";
 
 function App() {
-    return <AppRouter />;
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    return (
+        <>
+            <AppRouter isLoggedIn={isLoggedIn} />;
+            <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+        </>
+    );
 }
 
 export default App;
--- nwitter/src/components/Router.js
+++ nwitter/src/components/Router.js
@@ -1,10 +1,9 @@
-import { useState } from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 
-const AppRouter = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
+const AppRouter = ({ isLoggedIn }) => {
+    console.log(isLoggedIn);
     return (
         <Router>
             <Routes>
