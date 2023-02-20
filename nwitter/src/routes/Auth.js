8e08da8486fd6f33e982354ba33d05e5209ad588
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-02-20 15:33:07 +0900

    feat : auth 로그인 뼈대

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,3 +1,17 @@
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+    return (
+        <div>
+            <form>
+                <input type="email" placeholder="Email" required />
+                <input type="password" placeholder="Password" required />
+                <input type="submit" value="Log In" />
+            </form>
+            <div>
+                <button>Contunue With Google</button>
+                <button>Contunue With Github</button>
+            </div>
+        </div>
+    );
+};
 
 export default Auth;
