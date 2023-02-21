56ba1c7992221721b9087eeac54d7d6b57228132
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-02-21 16:30:00 +0900

    feat : 트윗 화면에 띄우기

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -6,24 +6,21 @@ const Home = () => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
 
-    // 트윗 가져오기
     const getNweets = async () => {
         const q = query(collection(dbService, "nweets"));
         const querySnapshot = await getDocs(q);
         querySnapshot.forEach((doc) => {
-            // 트윗 마다 아이디
             const nweetObj = {
                 ...doc.data(),
                 id: doc.id,
             };
-            // 트윗들
             setNweets((prev) => [nweetObj, ...prev]);
         });
     };
-
     useEffect(() => {
         getNweets();
     }, []);
+
     // 제출시
     const onSubmit = async (e) => {
         e.preventDefault();
@@ -50,6 +47,8 @@ const Home = () => {
         } = event;
         setNweet(value);
     };
+    console.log(nweets);
+
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -62,6 +61,13 @@ const Home = () => {
                 />
                 <input type="submit" value="Ntweet" />
             </form>
+            <div>
+                {nweets.map((nweet) => (
+                    <div key={nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
