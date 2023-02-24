0abafd3953fe12bcf2345e601d68fc26e0f108cc
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-02-24 14:27:05 +0900

    feat: 유저 본인 체크

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -86,7 +86,11 @@ const Home = ({ userObj }) => {
             <div>
                 {/* 트윗 뿌리기 */}
                 {nweets.map((nweet) => (
-                    <Nweet key={nweet.id} nweetObj={nweet} />
+                    <Nweet
+                        key={nweet.id}
+                        nweetObj={nweet}
+                        isOwner={nweet.creatorId === userObj.uid}
+                    />
                 ))}
             </div>
         </div>
